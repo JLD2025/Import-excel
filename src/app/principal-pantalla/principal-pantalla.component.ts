18889eaf684e32dcc37c3ffb23b11018ebcf68f4
Jacobo Lorente Diaz <49763919S@valmesa.net>
2025-04-16 13:11:15 +0200

    Cambios realizados en principal-pantalla.component.ts y .html

--- src/app/principal-pantalla/principal-pantalla.component.ts
+++ src/app/principal-pantalla/principal-pantalla.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,15 +6,33 @@ import { Router } from '@angular/router';
   templateUrl: './principal-pantalla.component.html',
   styleUrl: './principal-pantalla.component.css'
 })
-export class PrincipalPantallaComponent {
+export class PrincipalPantallaComponent implements OnInit {
   constructor(private router : Router){}
+  currentDate: string = "";
+
+  ngOnInit() {
+    this.updateTime();
+  }
+
+  updateTime(){
+    this.getCurrentDate();
+    if (typeof window !== 'undefined' && typeof window.requestAnimationFrame === 'function') {
+      window.requestAnimationFrame(() => this.updateTime()); // Llama de nuevo si estamos en un navegador
+    }
+  }
+
+  // Función para obtener la fecha y hora actuales
+  getCurrentDate() {
+    const ahora = new Date();
+    this.currentDate = ahora.toLocaleString('es-ES');
+  }
 
   AltaLotes(){
     this.router.navigate(['/alta-lotes'])
   }
 
   Conexion(){
-    this.router.navigate(['/inicio'])
+    this.router.navigate(['/alta-lotes'])
   }
 
 }
