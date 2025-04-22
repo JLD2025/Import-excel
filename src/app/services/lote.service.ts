dcb35cbb427a8dc183e44f83c912c0b074d61fc8
Jacobo Lorente Diaz <49763919S@valmesa.net>
2025-04-22 11:06:08 +0200

    C

--- src/app/services/lote.service.ts
+++ src/app/services/lote.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoteService {
-  private apiUrl = 'https://localhost:3939/api/lote/listado';
 
+export class LoteService {
   constructor(private http: HttpClient) {}
 
   obtenerLote(loteid: number) {
-    // Crear el objeto para el cuerpo de la solicitud
-    const body = { id: loteid };
-
-    // Configuración opcional de los headers (si es necesario)
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
-
-    return this.http.post(this.apiUrl, body, { headers });
+    return this.http.post('https://localhost:3939/api/lote/listado',{
+        id: loteid
+    });
   }
 }
