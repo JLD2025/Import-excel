9843adeab8d5616a8cb055efc8adeaad1f7c3b65
Jacobo Lorente Diaz <49763919S@valmesa.net>
2025-04-24 09:58:37 +0200

    Pequeños cambios en varios archivos como principal-pantalla.component.ts y .html y el .ts de excel-importer

--- src/app/excel-importer/excel-importer.component.ts
+++ src/app/excel-importer/excel-importer.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx'
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-excel-importer',
--- src/app/principal-pantalla/principal-pantalla.component.ts
+++ src/app/principal-pantalla/principal-pantalla.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoteService } from '../services/lote.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  imports: [FormsModule, CommonModule],
   selector: 'app-principal-pantalla',
   templateUrl: './principal-pantalla.component.html',
   styleUrl: './principal-pantalla.component.css'
@@ -10,6 +13,8 @@ import { LoteService } from '../services/lote.service';
 export class PrincipalPantallaComponent implements OnInit {
   constructor(private router : Router, private loteService: LoteService){}
   currentDate: string = "";
+  mostrarVentana: boolean = false;
+  idIngresado: number | null = null;
 
   ngOnInit() {
     this.updateTime();
@@ -33,19 +38,40 @@ export class PrincipalPantallaComponent implements OnInit {
   }
 
   Conexion() {
-    const loteid = 691;
-     console.log('Intentando hacer la conexión...');
-    this.loteService.obtenerLote(loteid).subscribe({
-      next: (res) => {
-        console.log('Respuesta del backend:', res);
-        //alert('Conexión exitosa: ' + JSON.stringify(res));
-        this.AltaLotes(res);
-        // Aquí podrías guardar los datos en una propiedad para mostrarlos en la vista
-      },
-      error: (err) => {
-        console.error('Error al conectar:', err);
-        alert('Error al conectar');
-      }
-    });
+    const errores: string[] = [];
+  
+    if (!this.idIngresado || isNaN(this.idIngresado) || this.idIngresado.toString().trim() === '') {
+      errores.push('No puede estar vacio.');
+    }
+
+    if (errores.length > 0) {
+      alert(errores);
+      return;
+    }
+
+    console.log('Intentando hacer la conexión...');
+    if(this.idIngresado !== null){
+      const loteid = this.idIngresado;
+      this.loteService.obtenerLote(loteid).subscribe({
+        next: (res) => {
+          console.log('Respuesta del backend:', res);
+          //alert('Conexión exitosa: ' + JSON.stringify(res));
+          this.AltaLotes(res);
+          // Aquí podrías guardar los datos en una propiedad para mostrarlos en la vista
+        },
+        error: (err) => {
+          console.error('Error al conectar:', err);
+          alert('Error al conectar');
+        }
+      });
+    }
   }
+
+  cancelarBusqueda() {
+    // Vaciamos los campos
+    this.idIngresado = null;
+  
+    // Cerramos los modales
+    this.mostrarVentana = false;
+  }  
 }
